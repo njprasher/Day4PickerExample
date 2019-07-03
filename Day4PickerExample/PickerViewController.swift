72bac90909d74990837469101316494d52a98a39
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-03 18:55:37 -0400

    [Neeraj] Created and implemented two picker views

--- Day4PickerExample/PickerViewController.swift
+++ Day4PickerExample/PickerViewController.swift
@@ -11,38 +11,59 @@ import UIKit
 class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var lblCourseName: UILabel!
+    @IBOutlet weak var lblCollegeName: UILabel!
     @IBOutlet weak var pickerCourseName: UIPickerView!
     var courseArray = ["Android Fundamentals", "iOS Fundamentals", "Swift Programming", "Database Design", "Programming in Java", "Web Development in PHP", "Machine Learning", "Data Analytics with R", "Learn Ruby - The Hard Way", "Data Structures in C++"]
+    
+    var collegeArray = ["Lambton College", "Humber College", "Sheridan College", "Seneca College", "Canadore College"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        self.pickerCourseName.delegate = self
+        self.pickerCourseName.dataSource = self
     }
     
     //added one component in picker
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     //added number of rows in picker same as number of values in array
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return courseArray.count
+        if component == 0{
+            return self.collegeArray.count
+        }else{
+            return self.courseArray.count
+        }
+        
     }
     
     //set value for each row
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return courseArray[row]
+        if component == 0{
+            return "\(row + 1) - \(self.collegeArray[row])"
+        }else{
+            return "\(row + 1) - \(self.courseArray[row])"
+        }
+        
     }
     
     //fetch the selected value
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        lblCourseName.text = courseArray[row]
+        
+        if component == 0{
+            self.lblCollegeName.text = self.collegeArray[row]
+        }else{
+            self.lblCourseName.text = self.courseArray[row]
+        }
+        
+        
     }
 }
 

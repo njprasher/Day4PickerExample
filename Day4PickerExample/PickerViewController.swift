eddf7e106230ddb223f04164fa0b7db2e24bcb6f
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-03 18:38:36 -0400

    [Neeraj] label displays value  for picker (not working yet)

--- Day4PickerExample/PickerViewController.swift
+++ Day4PickerExample/PickerViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class PickerViewController: UIViewController {
+class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var lblCourseName: UILabel!
-    
     @IBOutlet weak var pickerCourseName: UIPickerView!
+    var courseArray = ["Android Fundamentals", "iOS Fundamentals", "Swift Programming", "Database Design", "Programming in Java", "Web Development in PHP", "Machine Learning", "Data Analytics with R", "Learn Ruby - The Hard Way", "Data Structures in C++"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    //added one component in picker
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //added number of rows in picker same as number of values in array
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return courseArray.count
+    }
+    
+    //set value for each row
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return courseArray[row]
+    }
+    
+    //fetch the selected value
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lblCourseName.text = courseArray[row]
+    }
 }
 
